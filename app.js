fb4e8a227cd1e8c1a35cb816281051a3c17d042c
Katie <katiebutler37@gmail.com>
2022-06-27 13:26:39 -0400

    page template create

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-printProfileData(profileDataArgs);
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+// const printProfileData = profileDataArr => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+//   };
+// printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
